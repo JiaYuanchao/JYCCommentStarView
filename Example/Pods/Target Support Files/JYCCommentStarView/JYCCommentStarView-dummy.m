86075b6e4f340d63c498738e89fa264a9546ca25
jiayuanchao <9drug_jiayuanchao@9drug.com>
2017-05-15 16:11:50 +0800

    a

--- Example/Pods/Target Support Files/JYCCommentStarView/JYCCommentStarView-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_JYCCommentStarView : NSObject
-@end
-@implementation PodsDummy_JYCCommentStarView
-@end
--- Example/Pods/Target Support Files/Pods-JYCCommentStarView_Tests/Pods-JYCCommentStarView_Tests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_JYCCommentStarView_Tests : NSObject
-@end
-@implementation PodsDummy_Pods_JYCCommentStarView_Tests
-@end
